5f05e3acf5e3b3b181746548476e1423b2684d23
Bubu <gabrielmalbertin@gmail.com>
2025-03-24 21:11:36 +0100

    Added even more "minor changes"

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
+using System.Globalization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Drawing;
 
 namespace KMeansClusteringTest
 {
@@ -20,43 +17,39 @@ namespace KMeansClusteringTest
         private int k = 3;
         private List<ClusterPoint> ClusterPoints;
         private List<Centroid> Centroids;
-        Random rand = new Random();
+        private Random rand = new Random();
 
         string filePath = "data.csv";
-        
 
         public Form1()
         {
             InitializeComponent();
-            ClusterPoints = new List<ClusterPoint>(); // Initialize the list
+            ClusterPoints = new List<ClusterPoint>();
+            LoadData();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        private void LoadData()
+        {
             using (var reader = new StreamReader(filePath))
             using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 HasHeaderRecord = true
             }))
             {
-                var culture = new CultureInfo("en-US");
                 var records = csv.GetRecords<dynamic>();
                 foreach (var record in records)
                 {
-                    // Use the specified culture to parse the values correctly
-                    float x = float.Parse(record.X, culture); 
-                    float y = float.Parse(record.Y, culture); 
+                    // Adjust these values based on your CSV format
+                    float x = float.Parse(record.X, CultureInfo.InvariantCulture);
+                    float y = float.Parse(record.Y, CultureInfo.InvariantCulture);
                     ClusterPoints.Add(new ClusterPoint(x, y, 0));
                 }
             }
-            
-            this.Paint += new PaintEventHandler(Form1_Paint);
-            Task.Run(() => RunKMeans());
-
         }
 
         private async Task RunKMeans()
         {
-            foreach (var data in ClusterPoints)
-            {
-                data.ClusterID = 0;
-            }
             ClusterPoints = await Task.Run(() => KMeans(ClusterPoints, k));
             this.Invalidate();
         }
@@ -69,6 +62,7 @@ namespace KMeansClusteringTest
             if (ClusterPoints.Count == 0)
                 return;
 
+            // Calculate bounds to scale points accordingly
             float minX = ClusterPoints.Min(p => p.x);
             float maxX = ClusterPoints.Max(p => p.x);
             float minY = ClusterPoints.Min(p => p.y);
@@ -81,58 +75,10 @@ namespace KMeansClusteringTest
 
             float scaleX = (ClientSize.Width - 20) / rangeX;
             float scaleY = (ClientSize.Height - 20) / rangeY;
-            Console.WriteLine($"minX: {minX}, maxX: {maxX}, rangeX: {rangeX}, scaleX: {scaleX}");
-            Console.WriteLine($"minY: {minY}, maxY: {maxY}, rangeY: {rangeY}, scaleY: {scaleY}");
 
             foreach (var data in ClusterPoints)
             {
-                Brush PointBrush = Brushes.Black;
-                switch (data.ClusterID)
-                {
-                    case 1:
-                        PointBrush = Brushes.Red;
-                        break;
-                    case 2:
-                        PointBrush = Brushes.Green;
-                        break;
-                    case 3:
-                        PointBrush = Brushes.Blue;
-                        break;
-                    case 4:
-                        PointBrush = Brushes.Yellow;
-                        break;
-                    case 5:
-                        PointBrush = Brushes.Purple;
-                        break;
-                    case 6:
-                        PointBrush = Brushes.Orange;
-                        break;
-                    case 7:
-                        PointBrush = Brushes.Cyan;
-                        break;
-                    case 8:
-                        PointBrush = Brushes.Magenta;
-                        break;
-                    case 9:
-                        PointBrush = Brushes.Brown;
-                        break;
-                    case 10:
-                        PointBrush = Brushes.Pink;
-                        break;
-                    case 11:
-                        PointBrush = Brushes.LightBlue;
-                        break;
-                    case 12:
-                        PointBrush = Brushes.LightGreen;
-                        break;
-                    case 13:
-                        PointBrush = Brushes.LightYellow;
-                        break;
-                    case 0:
-                    default:
-                        PointBrush = Brushes.Gray; // Clusterless points as gray
-                        break;
-                }
+                Brush PointBrush = GetClusterColor(data.ClusterID);
 
                 float normalizedX = (data.x - minX) * scaleX + 10;
                 float normalizedY = (data.y - minY) * scaleY + 10;
@@ -141,10 +87,51 @@ namespace KMeansClusteringTest
             }
         }
 
+        private Brush GetClusterColor(int clusterId)
+        {
+            // Assign different colors to different clusters
+            switch (clusterId)
+            {
+                case 1:
+                    return Brushes.Red;
+                case 2:
+                    return Brushes.Green;
+                case 3:
+                    return Brushes.Blue;
+                case 4:
+                    return Brushes.Yellow;
+                case 5:
+                    return Brushes.Purple;
+                case 6:
+                    return Brushes.Orange;
+                case 7:
+                    return Brushes.Cyan;
+                case 8:
+                    return Brushes.Magenta;
+                case 9:
+                    return Brushes.Brown;
+                case 10:
+                    return Brushes.Pink;
+                default:
+                    return GetRandomColor(clusterId); // Default to a random color for higher cluster IDs
+            }
+        }
+
+        private Brush GetRandomColor(int clusterId)
+        {
+            Random random = new Random(clusterId);
+            return new SolidBrush(Color.FromArgb(random.Next(256), random.Next(256), random.Next(256)));
+        }
+
         private List<ClusterPoint> KMeans(List<ClusterPoint> dataset, int k)
         {
+            foreach (var data in ClusterPoints)
+            {
+                data.ClusterID = 0;
+            }
             Console.WriteLine("KMeans started");
             Centroids = new List<Centroid>();
+            // Initialize centroids by randomly selecting data points
             for (int i = 0; i < k; i++)
             {
                 int p = rand.Next(0, dataset.Count);
@@ -183,6 +170,7 @@ namespace KMeansClusteringTest
                     }
                 }
 
+                // Recalculate centroids
                 foreach (var centroid in Centroids)
                 {
                     centroid.recalculate(dataset);
@@ -192,34 +180,65 @@ namespace KMeansClusteringTest
             return dataset;
         }
 
-        private bool CheckDone(List<ClusterPoint> dataset)
+        private float getDistance(ClusterPoint a, ClusterPoint b)
         {
-            foreach (var data in dataset)
+            float dx = b.x - a.x;
+            float dy = b.y - a.y;
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        private async void buttonRunKMeans_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int newK) && newK > 0)
             {
-                if (data.ClusterID == 0)
+                k = newK; // Get k value from TextBox
+                await RunKMeans();
+            }
+        }
+
+        private async Task buttonRunElbowMethod_Click(object sender, EventArgs e)
+        {
+            int maxK = 10;
+            int optimalK = await Task.Run(() => FindOptimalK(ClusterPoints, maxK));
+            textBox1.Text = optimalK.ToString();
+        }
+
+        private int FindOptimalK(List<ClusterPoint> dataPoints, int maxK)
+        {
+            List<double> distortions = new List<double>();
+
+            for (int k = 1; k <= maxK; k++)
+            {
+                var clusters = KMeans(dataPoints, k);
+                distortions.Add(CalculateDistortion(dataPoints, clusters));
+            }
+
+            double minDistortion = distortions[0];
+            int optimalK = 1;
+
+            for (int i = 1; i < distortions.Count - 1; i++)
+            {
+                double slope1 = distortions[i] - distortions[i - 1];
+                double slope2 = distortions[i + 1] - distortions[i];
+                if (slope1 > 0 && slope2 < 0)
                 {
-                    return false;
+                    optimalK = i + 1;
+                    break;
                 }
             }
-            return true;
+
+            return optimalK;
         }
 
-       private float getDistance(ClusterPoint a, ClusterPoint b) 
-{ 
-    float dx = b.x - a.x;
-    float dy = b.y - a.y;
-    return (float)Math.Sqrt(dx * dx + dy * dy); // Use float instead of int
-}
-
-        private void Form1_Load(object sender, EventArgs e)
+        private double CalculateDistortion(List<ClusterPoint> dataPoints, List<ClusterPoint> clusters)
         {
-
+            double distortion = 0;
+            foreach (var point in dataPoints)
+            {
+                distortion += getDistance(point, clusters[point.ClusterID - 1]);
+            }
+            return distortion;
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e) { 
-        
-        }
-
         private async void button1_Click(object sender, EventArgs e)
         {
             if (int.TryParse(textBox1.Text, out int newK) && newK > 0)
@@ -230,13 +249,20 @@ namespace KMeansClusteringTest
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await buttonRunElbowMethod_Click(sender, e);
+            k = int.Parse(textBox1.Text);
+            await RunKMeans();
+            this.BeginInvoke(new Action(() => this.Invalidate()));
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
     }
-
-
-
 }
-
